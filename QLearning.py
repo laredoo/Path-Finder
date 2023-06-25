5515f722e405da4bbacb7a4bed2cb2b9b6d5ac95
Lucas Laredo <86807591+laredoo@users.noreply.github.com>
2023-06-25 19:42:51 -0300

    reward matrix set

--- QLearning.py
+++ QLearning.py
@@ -1,4 +1,6 @@
 import numpy as np
+import funcoes as f
+import sys
 
 def read_input_file(file_name):
     with open(file_name, 'r') as file:
@@ -25,20 +27,35 @@ def read_input_file(file_name):
     matrix = []
     for line in lines[2:]:
         row = list(map(int, line.strip().split()))
+        row += [0] * (dimensions - len(row))  # Preencher com zeros, se necessário
         matrix.append(row)
 
     return iterations, learning_rate, discount_factor, reward_value, epsilon, dimensions, np.array(matrix)
 
-# Exemplo de uso
-file_name = 'entrada.txt'  # Nome do arquivo de entrada
-iterations, learning_rate, discount_factor, reward_value, epsilon, dimensions, matrix = read_input_file(file_name)
+# Verificar os argumentos da linha de comando
+if len(sys.argv) < 3:
+    print("Uso: python3 TP2.py <arquivo_entrada> <arquivo_saida>")
+    sys.exit(1)
+
+# Obter os nomes dos arquivos de entrada e saída
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# Ler o arquivo de entrada
+iterations, learning_rate, discount_factor, reward_value, epsilon, dimensions, matrix = read_input_file(input_file)
+
+# inicializar o ambiente
+q_values = f.startEnvironment(dimensions, dimensions)
+
+# inicializar as recompensas e a posicao de inicio
+rewards, start_position = f.setRewards(matrix, reward_value)
+# print('start_position -------> ', start_position)
+
+# definir as acoes
+# 0 = up, 1 = right, 2 = down, 3 = left
+actions = ['up', 'right', 'down', 'left']
 
 # Imprimir os valores lidos
-print("Número de iterações:", iterations)
-print("Taxa de aprendizado (alpha):", learning_rate)
-print("Fator de desconto (gamma):", discount_factor)
-print("Valor da recompensa:", reward_value)
-print("Valor de epsilon (e):", epsilon)
-print("Dimensões da matriz:", dimensions)
-print("Matriz:")
-print(matrix)
+f.printData(iterations, learning_rate, discount_factor, 
+            reward_value, epsilon, dimensions, matrix)
+
