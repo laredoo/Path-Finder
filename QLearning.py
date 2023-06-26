aa669f7b447dbd247c1afc4c97c301581843d7ff
Lucas Laredo <86807591+laredoo@users.noreply.github.com>
2023-06-26 00:38:59 -0300

    several changes
    the algorithm isn't fully functional. Probabilly because it's accepting invalid moves.

--- QLearning.py
+++ QLearning.py
@@ -1,6 +1,9 @@
 import numpy as np
 import funcoes as f
 import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def read_input_file(file_name):
     with open(file_name, 'r') as file:
@@ -43,19 +46,63 @@ output_file = sys.argv[2]
 
 # Ler o arquivo de entrada
 iterations, learning_rate, discount_factor, reward_value, epsilon, dimensions, matrix = read_input_file(input_file)
+rows, columns = matrix.shape
 
 # inicializar o ambiente
-q_values = f.startEnvironment(dimensions, dimensions)
+q_values = f.startEnvironment(matrix)
 
 # inicializar as recompensas e a posicao de inicio
 rewards, start_position = f.setRewards(matrix, reward_value)
+start_row_index, start_column_index = start_position
+# print(rewards)
 # print('start_position -------> ', start_position)
+
+# print(q_values)
 
 # definir as acoes
 # 0 = up, 1 = right, 2 = down, 3 = left
 actions = ['up', 'right', 'down', 'left']
 
+# print(q_values)
+# print(rewards)
+
 # Imprimir os valores lidos
 f.printData(iterations, learning_rate, discount_factor, 
             reward_value, epsilon, dimensions, matrix)
 
+#run through 1000 training episodes
+for episode in range(iterations):
+    # posicao de inicio
+    row_index, column_index = start_position
+
+    # continue taking actions (i.e., moving) until we reach a terminal state
+    # (i.e., until we reach the +1 or -1 area or crash into a wall)
+    while not f.is_terminal_state(matrix, rewards, row_index, column_index):
+        # choose which action to take (i.e., where to move next)
+        action_index = f.get_next_action(q_values, row_index, column_index, epsilon)
+
+        # perform the chosen action, and transition to the next state (i.e., move to the next location)
+        old_row_index, old_column_index = row_index, column_index # store the old row and column indexes
+        row_index, column_index = f.get_next_location(matrix, row_index, column_index, action_index)
+
+        # proibindo posicoes fora da matriz
+        if row_index < 0 or row_index >= rows or column_index < 0 or column_index >= columns:
+            break
+
+        # receive the reward for moving to the new state, and calculate the temporal difference
+        reward = rewards[row_index, column_index]
+        old_q_value = q_values[old_row_index, old_column_index, action_index]
+        temporal_difference = reward + (discount_factor * np.max(q_values[row_index, column_index])) - old_q_value
+
+        # update the Q-value for the previous state and action pair
+        new_q_value = old_q_value + (learning_rate * temporal_difference) # Bellman Equation
+        q_values[old_row_index, old_column_index, action_index] = new_q_value
+
+print('Training complete!')
+
+# print(q_values)
+# print(f.get_shortest_path(matrix, rewards, start_row_index, start_column_index))
+
+fig = plt.figure()
+sns.heatmap(matrix, cbar = True, square= True, fmt='')
+plt.savefig(output_file)
--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,25 @@
 import numpy as np
 
-def startEnvironment(environment_rows, environment_columns):
+def startEnvironment(environment):
     #Create a 3D numpy array to hold the current Q-values for each state and action pair: Q(s, a) 
     #The array contains n rows and n columns (to match the shape of the environment), as well as a third "action" dimension.
     #The "action" dimension consists of 4 layers that will allow us to keep track of the Q-values for each possible action in
     #each state (see next cell for a description of possible actions). 
     #The value of each (state, action) pair is initialized to 0.
-    q_values = np.zeros((environment_rows, environment_columns, 4))
+    rows, columns = environment.shape
+    q_values = np.zeros((rows, columns, 4))
+    # agora, setamos a recompensa como +1, -1 e 0 nos terminais.
+    # Iterar sobre a matriz
+    for i in range(environment.shape[0]):
+        for j in range(environment.shape[1]):
+            value = environment[i, j]
+            if value == -1:
+                q_values[i,j] = 0
+            elif value == 7:
+                q_values[i,j] = 1 
+            elif value == 4:
+                q_values[i,j] = -1
+    
     return q_values
 
 def setRewards(environment, reward_value):
@@ -29,6 +42,99 @@ def setRewards(environment, reward_value):
     # aproveitamos para já definir a posicao de inicio do agente
     return rewards, start_position
 
+#define a function that determines if the specified location is a terminal state
+def is_terminal_state(environment, rewards, current_row_index, current_column_index):
+  rows, columns = environment.shape
+  if current_row_index < 0 or current_row_index >= rows or current_column_index < 0 or current_column_index >= columns:
+     return True
+  else:
+    #if the reward for this location is -1, then it is not a terminal state (i.e., it is a 'white square')
+    if rewards[current_row_index, current_column_index] == -1. or rewards[current_row_index, 
+                current_column_index] == 1. or rewards[current_row_index, current_column_index] == 0.:
+        return True
+    else:
+        return False
+  
+#define an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
+def get_next_action(q_values, current_row_index, current_column_index, epsilon):
+  #if a randomly chosen value between 0 and 1 is less than epsilon, 
+  #then choose the most promising value from the Q-table for this state.
+  if np.random.random() < epsilon:
+    return np.argmax(q_values[current_row_index, current_column_index])
+  else: #choose a random action
+    return np.random.randint(4)
+  
+#define a function that will get the next location based on the chosen action
+def get_next_location(environment, current_row_index, current_column_index, action_index):
+  rows, columns = environment.shape
+  # sorteamos um valor aleatorio entre 0 e 1
+  # se slide_factor < 80% nao escorrega
+  # caso contrario, sorteamos as outras opcoes
+  slide_factor = np.random.random()
+  actions = ['up', 'right', 'down', 'left']
+  new_row_index = current_row_index
+  new_column_index = current_column_index
+
+  if actions[action_index] == 'up' and current_row_index > 0:
+    if slide_factor < 0.8:
+        new_row_index -= 1
+    else:
+       if np.random.randint(2) == 0:
+          new_column_index += 1 # escorrego para direita
+       new_column_index -= 1 # escorrego para esquerda
+
+  elif actions[action_index] == 'right' and current_column_index < columns - 1:
+    if slide_factor < 0.8:    
+        new_column_index += 1
+    else:
+       if np.random.randint(2) == 0:
+          new_row_index -= 1 # escorrego para cima
+       new_row_index += 1 # escorrego para baixo
+
+  elif actions[action_index] == 'down' and current_row_index < rows - 1:
+    if slide_factor < 0.8:
+        new_row_index += 1
+    else:
+       if np.random.randint(2) == 0:
+          new_column_index += 1 # escorrego para direita
+       new_column_index -= 1 # escorrego para esquerda
+
+  elif actions[action_index] == 'left' and current_column_index > 0:
+    if slide_factor < 0.8:
+        new_column_index -= 1
+    else:
+       if np.random.randint(2) == 0:
+          new_row_index -= 1 # escorrego para cima
+       new_row_index += 1 # escorrego para baixo
+
+  return new_row_index, new_column_index
+
+#Define a function that will get the shortest path between any location within the warehouse that 
+#the robot is allowed to travel and the item packaging location.
+def get_shortest_path(environment, rewards, start_row_index, start_column_index):
+  rows, columns = environment.shape
+  #return immediately if this is an invalid starting location
+  if is_terminal_state(environment, rewards, start_row_index, start_column_index):
+    return []
+  else: #if this is a 'legal' starting location
+    current_row_index, current_column_index = start_row_index, start_column_index
+    shortest_path = []
+    shortest_path.append([current_row_index, current_column_index])
+    #continue moving along the path until we reach the goal (i.e., the item packaging location)
+    while not is_terminal_state(environment, rewards, current_row_index, current_column_index):
+      #get the best action to take
+      action_index = get_next_action(environment, current_row_index, current_column_index, 1)
+      
+      #move to the next location on the path, and add the new location to the list
+      current_row_index, current_column_index = get_next_location(environment, current_row_index, current_column_index, action_index)
+      
+      # proibindo posicoes fora da matriz
+      if current_row_index < 0 or current_row_index >= rows or current_column_index < 0 or current_column_index >= columns:
+        break
+      
+      shortest_path.append([current_row_index, current_column_index])
+    return shortest_path
+
 def printData(i, alpha, gamma, r, epsilon, d, matrix):
     print("Número de iterações:", i)
     print("Taxa de aprendizado (alpha):", alpha)
@@ -36,6 +142,6 @@ def printData(i, alpha, gamma, r, epsilon, d, matrix):
     print("Valor da recompensa:", r)
     print("Valor de epsilon (e):", epsilon)
     print("Dimensões da matriz:", d)
-    print("Matriz:")
-    print(matrix)
+    # print("Matriz:")
+    # print(matrix)
 
